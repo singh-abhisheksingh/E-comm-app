24b0939fad93d50cf4227fe60140ee5adcda52b3
singh-abhisheksingh <abhisheksingh0058@gmail.com>
2024-09-04 23:31:41 +0530

    Initial setup commit

---
+++ src/test/java/com/lucifer/e_com_app/EComAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucifer.e_com_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
